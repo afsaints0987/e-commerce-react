367e37a805d74282e44ea8fe81447ea98d784c47
afsaints0987 <saints0987@gmail.com>
2023-05-07 16:22:34 +0800

    update cart components

--- snapmart/src/App.tsx
+++ snapmart/src/App.tsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
 import Navigation from "./components/Navigation/Navigation";
 import ProductItems from "./components/Products/ProductItems";
 import items from "./data/items.json";
@@ -6,9 +6,8 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Category from "./components/Sidebar/Category";
 import Cart from "./components/Cart/Cart";
-import Modal from 'react-bootstrap/Modal'
-import { Items } from './types/Items';
-
+import Modal from "react-bootstrap/Modal";
+import { Items } from "./types/Items";
 
 const CheckoutModal = (props: any) => {
   return (
@@ -20,73 +19,132 @@ const CheckoutModal = (props: any) => {
       className="px-4 py-2"
     >
       <Modal.Body>
-        <h3 className='text-center'>Thank you for purchasing!</h3>
+        <h3 className="text-center">Thank you for purchasing!</h3>
       </Modal.Body>
     </Modal>
   );
-}
-
+};
 
 const App: React.FC = () => {
-  const [productList, setProductList] = useState<Items[]>(items)
-  const [cartItems, setCartItems] = useState<Items[]>(items)
-  const [showModal, setShowModal] = useState<boolean>(false)
+  const [productList, setProductList] = useState<Items[]>(items);
+  const [cartItems, setCartItems] = useState<Items[]>(items);
+  const [showModal, setShowModal] = useState<boolean>(false);
+
   
- const handleAddToCart = (item: Items, quantity: number) => {
-   const cartItemIndex = cartItems.findIndex(
-     (cartItem) => cartItem.id === item.id
-   );
+  const handleAddToCart = (item: Items, quantity: number) => {
+    const cartItemIndex = cartItems.findIndex(
+      (cartItem) => cartItem.id === item.id
+    );
 
-   if (cartItemIndex === -1) {
-     const cartItem = {
-       ...item,
-       quantity: quantity,
-       totalPrice: item.unitPrice * quantity,
-     };
-     setCartItems((prevItems) => {
-       const updatedCartItems = [cartItem, ...prevItems];
-       localStorage.setItem("cart-items", JSON.stringify(updatedCartItems));
-       return updatedCartItems;
-     });
-   }
- };
-
-  const handleCategoryList = (item?: string) => {
-    const ProductsByCategory = items.filter(product => product.category === item) 
-    setProductList(ProductsByCategory)
-  }
-  
-  const handleModal = () => {
-    setShowModal(true)
-    setTimeout(() => {
-      setShowModal(false)
-      clearCart()
-    },3000)
-  }
-
-  const handleIncrement = () => {
-    console.log('this is increment')
+    if (cartItemIndex === -1) {
+      const cartItem = {
+        ...item,
+        quantity: quantity,
+        totalPrice: item.unitPrice * quantity,
+      };
+      setCartItems((prevItems) => {
+        const updatedCartItems = [cartItem, ...prevItems];
+        localStorage.setItem("cart-items", JSON.stringify(updatedCartItems));
+        return updatedCartItems;
+      });
+    } else {
+      const updatedCartItems = cartItems.map((cartItem) => {
+        if (cartItem.id === item.id) {
+          return {
+            ...cartItem,
+            quantity: cartItem.quantity + quantity,
+            totalPrice: (cartItem.quantity + quantity) * cartItem.unitPrice,
+          };
+        }
+        return cartItem;
+      });
+      setCartItems(updatedCartItems);
+      localStorage.setItem("cart-items", JSON.stringify(updatedCartItems));
+    }
   };
 
-  const handleDecrement = () => {
-    console.log('this is decrement')
+  const handleCategoryList = (item?: string) => {
+    const ProductsByCategory = items.filter(
+      (product) => product.category === item
+    );
+    setProductList(ProductsByCategory);
+  };
+  const handleDelete = (item: Items) => {
+    const updatedCartItems = cartItems.filter(
+      (cartItem) => cartItem.id !== item.id
+    );
+    setCartItems(updatedCartItems);
+    localStorage.setItem("cart-items", JSON.stringify(updatedCartItems));
+  };
+
+  const handleModal = () => {
+    setShowModal(true);
+    setTimeout(() => {
+      setShowModal(false);
+      clearCart();
+    }, 3000);
   };
 
   const clearCart = () => {
     setCartItems([]);
-    localStorage.removeItem('cart-items')
-  }
-
-  const cartItemPrice = cartItems.map(cart => {
-    return Number(cart.unitPrice);
-  }) 
-
-  useEffect(() => {
-    const cartItems = JSON.parse(localStorage.getItem('cart-items') || '[]')
-    setCartItems(cartItems)
-  },[])
+    localStorage.removeItem("cart-items");
+  };
 
   
+  const totalPrice = cartItems.reduce((total, item) => {
+    return total + Number(item.unitPrice) * Number(item.quantity);
+  }, 0);
+
+  const totalQuantity = cartItems.reduce((total, item) => {
+    return total + Number(item.quantity);
+  }, 0);
+
+
+  const handleIncrement = (item: Items, quantity: number) => {
+    const updatedCartItems = cartItems.map((cartItem) => {
+      if (cartItem.id === item.id) {
+        const updatedQuantity = cartItem.quantity + quantity;
+        const updatedTotalPrice = updatedQuantity * cartItem.unitPrice;
+        return {
+          ...cartItem,
+          quantity: updatedQuantity,
+          totalPrice: updatedTotalPrice,
+        };
+      }
+      return cartItem;
+    });
+    setCartItems(updatedCartItems);
+    localStorage.setItem("cart-items", JSON.stringify(updatedCartItems));
+  };
+
+  const handleDecrement = (item: Items, quantity: number) => {
+    const updatedCartItems = cartItems
+      .map((cartItem) => {
+        if (cartItem.id === item.id) {
+          const updatedQuantity = cartItem.quantity - quantity;
+          if (updatedQuantity <= 0) {
+            return null;
+          } else {
+            const updatedTotalPrice = updatedQuantity * cartItem.unitPrice;
+            return {
+              ...cartItem,
+              quantity: updatedQuantity,
+              totalPrice: updatedTotalPrice,
+            };
+          }
+        }
+        return cartItem;
+      })
+      .filter((item) => item !== null);
+    setCartItems(updatedCartItems);
+    localStorage.setItem("cart-items", JSON.stringify(updatedCartItems));
+  };
+
+  useEffect(() => {
+    const cartItems = JSON.parse(localStorage.getItem("cart-items") || "[]");
+    setCartItems(cartItems);
+  }, []);
+
   return (
     <div>
       <Navigation />
@@ -104,14 +162,12 @@ const App: React.FC = () => {
           <Cart
             cartItems={cartItems}
             checkout={handleModal}
-            itemDecrement={handleIncrement}
-            itemIncrement={handleDecrement}
+            itemDecrement={handleDecrement}
+            itemIncrement={handleIncrement}
+            deleteItem={(item) => handleDelete(item)}
             clearCart={() => clearCart()}
-            quantity={cartItems.length}
-            priceItem={cartItemPrice.reduce(
-              (total, price) => total + price,
-              0
-            )}
+            quantity={totalQuantity}
+            priceItem={totalPrice}
           />
         </Col>
       </Row>
